1e5e612fe54a2c75386fa26bd44784ebe5a3b337
bindie <bindiethebot@gmail.com>
2020-09-29 21:55:48 +0200

    chore: modified base url

--- src/recoil/selectors/index.ts
+++ src/recoil/selectors/index.ts
@@ -1,18 +1,12 @@
 import axios from "axios";
 import { selectorFamily } from "recoil";
 
-const getBaseUrl = () => {
-  if (process.env.NODE_ENV === "development") {
-    return process.env.REACT_APP_SERVER_BASE_URL_TESTNET;
-  } else {
-    return process.env.REACT_APP_SERVER_BASE_URL;
-  }
-};
+const baseUrl = process.env.REACT_APP_SERVER_BASE_URL;
 
 export const transactionHistoryQuery = selectorFamily({
   key: "transactionHistoryQuery",
   get: ({ page, limit }: { page: number; limit: number }) => async () => {
-    const response = await axios.get(`${getBaseUrl()}/transactions`, {
+    const response = await axios.get(`${baseUrl}/transactions`, {
       params: {
         page,
         limit
@@ -29,7 +23,7 @@ export const transactionHistoryQuery = selectorFamily({
 export const voterOverviewQuery = selectorFamily({
   key: "voterOverviewQuery",
   get: ({ page, limit }: { page: number; limit: number }) => async () => {
-    const response = await axios.get(`${getBaseUrl()}/simple`, {
+    const response = await axios.get(`${baseUrl}/simple`, {
       params: {
         page,
         limit
